7c89dc88a1dfd53278236a46245e64a2ac3926d8
DarioOrbita <104122781+DarioOrbita@users.noreply.github.com>
2022-07-27 13:57:27 -0500

    add inquirer and first prompt

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
 
+// fs.writeFile('./index.html', pageHTML, err => {
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-
-    if(err) throw new Error(err);
-
-    console.log('portfolio complete! check index.html to see the output!');
+//     if(err) throw err;
     
-});
+// });
+
+inquirer.prompt([
+    {
+
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?',
+
+    }
+])
+    .then(answers => console.log(answers));
